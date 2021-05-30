351b0d417f9c37da742b6e056e7a7778844b6de0
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2021-05-29 21:18:42 -0400

    project setup: Hello World

---
+++ sonar-project.js
@@ -0,0 +1,13 @@
+// இது என்ன ?
+// சொல்லிடாதீங்க அடிச்சு கூடா கேப்பாங்க அப்பவோம் சொல்லிடாதீங்க
+const sonarqubeScanner = require('sonarqube-scanner');
+sonarqubeScanner(
+    {
+        serverUrl: 'http://localhost:9000',
+        options: {
+            'sonar.sources': 'routes, app.js, views, public, bin',
+            'sonar.inclusions': '**', // Entry point of your code
+            'sonar.test.inclusions': '',
+            'sonar.testExecutionReportPaths': ''
+        }
+    }, () => { });
